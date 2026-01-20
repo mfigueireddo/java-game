19f67be030d57910fd7cf72c734b885beaac1450
Matheus Figueiredo <108003682+mfigueireddo@users.noreply.github.com>
2026-01-20 17:43:41 -0300

    Separated Panel from Menu

--- src/controller/GameRunner.java
+++ src/controller/GameRunner.java
@@ -5,7 +5,6 @@ import view.Window;
 public class GameRunner {
 
     static Window window;
-
     public static void main(String[] args) {
         window = Window.GetInstance();
         window.ShowMenu();
--- src/view/Menu.java
+++ src/view/Menu.java
@@ -1,13 +1,8 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Toolkit;
-import javax.swing.JPanel;
-import management.ErrorStatus;
-import management.Wrappers;
 
-public class Menu extends JPanel{
+public class Menu extends Panel {
 
     private static Menu instance;
 
@@ -18,23 +13,8 @@ public class Menu extends JPanel{
         return instance;
     }
 
-    private static final int MIN_WIDTH = 800;
-    private static final int MIN_HEIGHT = 600;
-
-    private Menu(){
-        Dimension screen_size = Toolkit.getDefaultToolkit().getScreenSize();
-        int screen_width = screen_size.width;
-        int screen_height = screen_size.height;
-        try {
-            Wrappers.VerifyMinValue(screen_width, MIN_WIDTH);
-            Wrappers.VerifyMinValue(screen_height, MIN_HEIGHT);
-        } 
-        catch (Exception e) {
-            System.err.println("Error: " + e.getMessage());
-            System.exit(ErrorStatus.MIN_SCREEN_SIZE.GetExitCode());
-        }
-        this.setPreferredSize(new Dimension(screen_width, screen_height));
-        
+    private Menu() {
+        super();
         this.setBackground(Color.BLACK);
     }
 
